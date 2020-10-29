2156bc6c5527164b61f4bbd5ea6d4ec9ff3524ab
O'Hara <lohara@udel.edu>
2020-10-28 23:16:56 -0400

    finished number utilities

--- src/main/java/com/returnready/loopthereitis/NumberUtilities.java
+++ src/main/java/com/returnready/loopthereitis/NumberUtilities.java
@@ -3,38 +3,68 @@ package com.returnready.loopthereitis;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String returnedRange="";
+        for(int currentNumberInRange = (start %2==0)?start:start+1; currentNumberInRange < stop;
+            currentNumberInRange+=2){
+            returnedRange = returnedRange + currentNumberInRange;
+        }
+        return returnedRange;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+
+        String returnedRange="";
+        for(int currentNumberInRange = (start %2!=0)?start:start+1; currentNumberInRange < stop;
+            currentNumberInRange+=2){
+            returnedRange = returnedRange + currentNumberInRange;
+        }
+        return returnedRange;
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        String returnedRange="";
+        for(int currentNumberInRange = start; currentNumberInRange< stop;
+            currentNumberInRange=currentNumberInRange + step) {
+            returnedRange = returnedRange + currentNumberInRange * currentNumberInRange;
+        }
+        return returnedRange;
     }
 
     public static String getRange(int stop) {
         String returnedRange = "";
-        for(int currentNumberInRange = 0; currentNumberInRange < stop - 1; currentNumberInRange++){
+        for(int currentNumberInRange = 0; currentNumberInRange < stop; currentNumberInRange++){
             returnedRange = returnedRange + currentNumberInRange;
         }
         return returnedRange;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
-    }
+        String returnedRange="";
+        for(int currentNumberInRange = start; currentNumberInRange< stop; currentNumberInRange++){
+            returnedRange = returnedRange + currentNumberInRange;
+        }
+        return returnedRange;
+         }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String returnedRange="";
+        for(int currentNumberInRange = start; currentNumberInRange< stop;
+          currentNumberInRange=currentNumberInRange + step) {
+            returnedRange = returnedRange + currentNumberInRange;
+        }
+        return returnedRange;
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        String returnedRange="";
+        for(int currentNumberInRange = start; currentNumberInRange< stop;
+            currentNumberInRange=currentNumberInRange + step) {
+            returnedRange = returnedRange + (int) (Math.pow(currentNumberInRange, exponent));
+        }
+        return returnedRange;
     }
 }
